247ba46538f70723ff7a1fa7435887b3b89ccabe
Joel Jones <drinkh20bois@gmail.com>
2024-02-25 00:36:30 -0800

    added some js events

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function Click() {
+  alert("You just clicked the title!");
+}
+
+function mouseenter() {
+  document.getElementById("words").style.color = "blue";
+}
+
+function mouseleave() {
+  document.getElementById("words").style.color = "black";
+}
