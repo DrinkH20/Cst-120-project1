40e0a9e99c4d2e6c23617a719206aedb54123c81
Joel Jones <drinkh20bois@gmail.com>
2024-02-29 14:37:25 -0800

    fixed the swup animation and the regular java animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-import Swup from "https://unpkg.com/swup@4?module";
-const swup = new Swup();
-
 function Click() {
   alert("You just clicked the title!");
 }
 
 function mouseEnter() {
-  document.getElementById("words").style.color = "rgb(200, 20, 120)";
+  document.getElementById("words").style.color = "rgb(20, 20, 120)";
 }
 
 function mouseLeave() {
